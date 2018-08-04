729321737191f71d62bd155a57f6ec0a08298d1f
vrajasekaran <vprajasekaran@gmail.com>
2018-08-04 11:14:43 -0400

    config

--- ethereum/truffle-config.js
+++ ethereum/truffle-config.js
@@ -2,16 +2,11 @@ const HDWalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
     migrations_directory: "./migrations",
     networks: {
-      // development: {
-      //   host: "localhost",
-      //   port: 7545,
-      //   network_id: "*" // Match any network id
-      // },
-      rinkeby: {
-        provider: new HDWalletProvider("lizard around pond million income buffalo kick best volume brown blossom old", "https://rinkeby.infura.io/zDgeQjWC1bsqjYMRpJqH", 4),
-        network_id: '3'            
-       
-        }
+      development: {
+        host: "localhost",
+        port: 7545,
+        network_id: "*" // Match any network id
+      }
     }
   };
   
